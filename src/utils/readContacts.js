90b2c5b25cf4628c2f89789dd6312d2261de74dd
Alexey <mirar2246@gmail.com>
2024-11-12 00:03:31 +0200

    ad

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,7 +1,7 @@
 import { PATH_DB } from '../constants/contacts.js';
 import fs from 'node:fs/promises';
 
-export const getAllContacts = async () => {
+export const readContacts = async () => {
   try {
     const data = await fs.readFile(PATH_DB, { encoding: 'utf-8' });
     return data;
